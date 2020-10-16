fdc64ef39daebf5c39ecb36d0b82162196f7bd4d
gnarlee-gee <charlesgrealy@gmail.com>
2020-10-15 21:47:16 -0500

    reduce size in order to reduce lag

--- script/script.js
+++ script/script.js
@@ -23,17 +23,15 @@ container.classList.add('container');
 wrapper.appendChild(container);
 
 container.setAttribute('style',
-    `width: 50vw;
-                        height: 80vh;
-                        background-color: #e6e6e6;
+                       `background-color: #e6e6e6;
                         display: grid;
-                        grid-template-columns: repeat(auto-fill, 20px);
-                        grid-template-rows: 20px 20px repeat(auto-fill, 20px)`);
+                        grid-template-columns: repeat(15, 30px);
+                        grid-template-rows: 20px 20px repeat(16, 30px)`);
 
 // creates 1200 divs
 let counter = 1;
-for (let i = 1; i < 49; i++) {
-    for (let j = 3; j < 28; j++) {
+for (let i = 1; i < 16; i++) {
+    for (let j = 3; j < 19; j++) {
         const div = document.createElement('div');
         div.classList.add(`item-${counter++}`);
         div.setAttribute('style',
@@ -49,20 +47,24 @@ wrapper.addEventListener('mouseup', function () {
     isDrawing = false;
 })
 
-let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-randomColor = "#" + randomColor;
+let r = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+let g = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+let b = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+ 
+let randomColor = `rgb(${r + ", " + g + ", " + b})`;
+console.log(randomColor)
 
 container.addEventListener('mouseenter', function () {
     if (!isDrawing) {
         const nodes = document.querySelectorAll('.container > *');
-        for (let i = 0; i < nodes.length - 2; i++) {
+        for (let i = 0; i < nodes.length - 3; i++) {
             nodes[i].addEventListener('mouseenter', function () {
-                if (nodes[i].style.backgroundColor != randomColor) {} else {
+                if (nodes[i].style.backgroundColor == randomColor) {} else {
                     nodes[i].style.backgroundColor = '#363636';
                 }
             })
             nodes[i].addEventListener('mouseleave', function () {
-                if (nodes[i].style.backgroundColor != randomColor) {} else {
+                if (nodes[i].style.backgroundColor == randomColor) {} else {
                     setTimeout(function () {
                         nodes[i].style.backgroundColor = '#e6e6e6';
                     }, 50);
@@ -75,12 +77,12 @@ container.addEventListener('mouseenter', function () {
 container.addEventListener('mousedown', function () {
     isDrawing = true;
     const nodes = document.querySelectorAll('.container > *');
-    for (let i = 0; i < nodes.length - 2; i++) {
+    for (let i = 0; i < nodes.length - 3; i++) {
         nodes[i].addEventListener('click', function () {
-            nodes[i].style.backgroundColor = "#" + randomColor;
+            nodes[i].style.backgroundColor = randomColor;
         })
         nodes[i].addEventListener('dblclick', function () {
-            nodes[i].style.backgroundColor = "#" + randomColor;
+            nodes[i].style.backgroundColor = randomColor;
         })
         nodes[i].addEventListener('mouseenter', function () {
             if (isDrawing === true) {
@@ -100,13 +102,13 @@ container.addEventListener('mouseup', function () {});
 const firstRow = document.createElement('div');
 firstRow.classList.add('first-row');
 container.appendChild(firstRow);
-firstRow.setAttribute('style', 'grid-column: 1/49; grid-row: 1/3; background-color: white;')
+firstRow.setAttribute('style', 'grid-column: 1/16; grid-row: 1/3; background-color: white;')
 
 const clearBtn = document.createElement('div');
 clearBtn.classList.add('clear-btn');
 
 clearBtn.setAttribute('style',
-    `grid-column: 4/8;
+    `grid-column: 2/5;
                         grid-row: 1/3;
                         display: grid;
                         align-items: center; 
@@ -132,7 +134,7 @@ const colors = document.createElement('div');
 colors.classList.add('colors');
 
 colors.setAttribute('style',
-                    `grid-column: 21/25;
+                    `grid-column: 5/8;
                     grid-row: 1/3;
                     display: grid;
                     align-items: center; 
